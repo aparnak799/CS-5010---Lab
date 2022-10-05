7c43a34349d03d6c63be6858ab23eb05d5b8d92f
Aparna Krishnan <aparna.k799@gmail.com>
2022-10-04 21:12:33 -0700

    Initial commit - began class design

---
+++ src/Bird.java
@@ -0,0 +1,10 @@
+public class Bird {
+    public int birdID;
+    public String birdType;
+    public int noOfWings;
+    public boolean isExtinct;
+    public String[] characteristics;
+    public String[] preferredFood;
+
+
+}
